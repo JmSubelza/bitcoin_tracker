6e9663e40ae39c191a4794941cc6ef1dbfb0dc08
Juan David Hernandez G <juandavid.hg@digitalsmark.com>
2015-12-15 20:46:34 -0500

    change models PriceHistory

--- historical_data/models.py
+++ historical_data/models.py
@@ -7,6 +7,7 @@ from django.db import models
 
 class PriceHistory(models.Model):
     date = models.DateTimeField(auto_now_add=True)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
     volume = models.PositiveIntegerField()
     total_btc = models.PositiveIntegerField()
+    market_cap = models.PositiveIntegerField(null=True)
